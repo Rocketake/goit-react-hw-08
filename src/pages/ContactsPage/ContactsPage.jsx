a2ac8157b6a566042538bfa45c7e5c53cd3c87e2
nazarufakit <onazik08072005@gmail.com>
2024-12-28 14:52:34 +0200

    fix

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,8 +1,17 @@
 import ContactForm from '../../components/ContactForm/ContactForm'
 import SearchBox from '../../components/SearchBox/SearchBox'
 import ContactList from '../../components/ContactList/ContactList'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from '../../redux/contacts/operations';
 
 const ContactsPage = () => {
+  const dispatch = useDispatch()
+
+ useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
 	<div><ContactForm />
       <SearchBox/>
